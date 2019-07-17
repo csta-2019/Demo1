3b2578a1116b1404518c24553b7ede45aef1c412
mwschmitt1 <49175104+mwschmitt1@users.noreply.github.com>
2019-07-17 10:41:34 -0400

    Update colors.py
    Changed age

--- colors.py
+++ colors.py
@@ -3,5 +3,5 @@ colors = ['red','yellow','green','blue']
 i = 0
 
 while i < len(colors):
-    print("When I was %d my favorite color was %s" % (i,colors[i]))
+    print("When I was %d my favorite color was %s" % (i*4,colors[i]))
     i+=1
